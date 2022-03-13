89954b0fbe1bcd9b2e63c8bbab1a1f3d525eaf45
ankitkrchy <ankitkrchy@gmail.com>
2022-03-13 14:19:01 +0530

    Redis-Integration

--- src/main/java/com/chatapplication/chat/model/User.java
+++ src/main/java/com/chatapplication/chat/model/User.java
@@ -1,5 +1,8 @@
 package com.chatapplication.chat.model;
 
+import org.springframework.data.redis.core.RedisHash;
+
+@RedisHash("User")
 public class User {
     private int userID;
     private String userName;
